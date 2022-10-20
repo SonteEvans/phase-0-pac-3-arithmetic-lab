11199e51b51fa4f5f53f7045d01c56906ccf0575
Jahsonte Evans <jahsonteevans@gmail.com>
2022-10-19 20:20:18 -0600

    commit tree 084014a63be975857a83ac138ad1d1bd378312df

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(c, d) {
+    return c - d
+}
+
+function multiply(e, f) {
+    return e * f
+}
+
+function divide(g, h) {
+    return g / h
+}
+
+function increment(q) {
+    return (q + 1)
+}
+
+function decrement(x) {
+    return (x - 1)
+}
+
+function makeInt(z) {
+    
+    return parseFloat(z)
+}
+
+function preserveDecimal(j) {
+    return parseFloat(j)
+}
